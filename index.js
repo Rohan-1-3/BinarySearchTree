86ab507c7c81d08bae9154d6ff4e98940cbc16cc
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-02-04 12:45:31 +0545

    added find and return the respective node

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable class-methods-use-this */
 import mergeSort from "./mergeSort";
 import Node from "./node";
@@ -29,10 +30,10 @@ const Tree = class{
     }
 
     insert(data, currentNode = this.root) {
-        if (currentNode === null) return new Node(data);
-        if (currentNode.data === data) return false;
+        if (currentNode === null) return new Node(data);// creates new node at the end
+        if (currentNode.data === data) return false;// no dupes
     
-        if (currentNode.data < data) {
+        if (currentNode.data < data) {// newData greater than current nodeData then node changes to node's right side
           currentNode.right = this.insert(data, currentNode.right);
         } else {
           currentNode.left = this.insert(data, currentNode.left);
@@ -43,31 +44,32 @@ const Tree = class{
     remove(data, currentNode = this.root){
         if(currentNode === null) return currentNode;
 
-        if (currentNode.data < data) {
+        if (currentNode.data < data) {// comparing the removed data in the tree and finding its position
             currentNode.right = this.remove(data, currentNode.right);
         } else {
             currentNode.left = this.remove(data, currentNode.left);
         }
 
-        if(currentNode.data === data){
+        if(currentNode.data === data){// data mathces in tree
             if(currentNode.left===null && currentNode.right === null){
-                return null;
+                return null;// removes the leaf of the tree
             }
-            if(currentNode.left === null){
+            if(currentNode.left === null){// if has one child makes the child the parent
                 return currentNode.right;
             }
             if(currentNode.right === null){
                 return currentNode.left;
             }
-            currentNode.data =  this.minValue(currentNode.right);
-
+            // function for a parent having both children
+            currentNode.data =  this.minValue(currentNode.right);// changes the removed nodeData to the next biggest number
+            // removing the sucessor after replacement
             currentNode.right = this.remove(currentNode.data, currentNode.right);
         }
 
         return currentNode;
     }
 
-    minValue(node){
+    minValue(node){// returns the least value when a removed node has both side
         if(node.left === null) return node.data;
         
         node = this.minValue(node.left);
@@ -75,32 +77,45 @@ const Tree = class{
         return node;
     }
     
-    preOrder(node){
+    preOrder(node){// display depth 1st
         if (node == null) return;
         console.log(`${node.data}`);
         this.preOrder(node.left);
         this.preOrder(node.right);
     }
     
-    inOrder(node){
+    inOrder(node){// display depth 1st
         if (node == null) return;
         this.preOrder(node.left);
         console.log(`${node.data}`);
         this.preOrder(node.right);
     }
     
-    postOrder(node){
+    postOrder(node){// display depth 1st
         if (node == null) return;
         this.preOrder(node.left);
         this.preOrder(node.right);
         console.log(`${node.data}`);
     }
 
-    sort(arr){
+    sort(arr){// removes dupes and then sorts the array
         const noDuplicates = [...new Set(arr)];
         const sortedArray = mergeSort(noDuplicates);
         return sortedArray;
     }
+
+    find(data,  currentNode = this.root){
+        if(currentNode === null) return currentNode;
+        if(currentNode.data === data) return console.log(currentNode);
+
+        if (currentNode.data < data) {// comparing the removed data in the tree and finding its position
+            currentNode.right = this.find(data, currentNode.right);
+        } else {
+            currentNode.left = this.find(data, currentNode.left);
+        }
+        return currentNode;
+    }
+ 
 }
 
 // const randomArray = (size) => Array.from({ length: size }, () => Math.floor(Math.random() * 100))
@@ -113,8 +128,9 @@ binaryTree.prettyPrint();
 binaryTree.insert(69);
 // binaryTree.insert(68);
 // binaryTree.insert(20);
-// binaryTree.insert(6346);
-// binaryTree.insert(6350);
+binaryTree.insert(6346);
+binaryTree.insert(6350);
 // binaryTree.prettyPrint();
-binaryTree.remove(28);
-binaryTree.prettyPrint();
+// binaryTree.remove(28);
+// binaryTree.prettyPrint();
+binaryTree.find(6346)
